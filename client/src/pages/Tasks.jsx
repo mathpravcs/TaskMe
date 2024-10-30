03fe5eaa54918d6a661f51e027df9650e25fb159
mathpravcs <matheus_n11@live.com>
2024-10-29 23:48:57 -0300

    adicionando a pagina de tasks

--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -1,4 +1,3 @@
-import { CircularProgress } from "@mui/material";
 import React from "react";
 import { FaList } from "react-icons/fa";
 import Loading from "../components/Loader";
@@ -6,8 +5,14 @@ import Loading from "../components/Loader";
 import { MdGridView } from "react-icons/md";
 import { useParams } from "react-router-dom";
 import Title from "../components/Title";
+import Button from "../components/Button";
+import { IoMdAdd } from "react-icons/io";
+import Tabs from "../components/Tabs";
+import TaskTitle from "../components/TaskTitle";
+import BoardView from "../components/BoardView";
+import { tasks } from "../assets/data";
 
-const Tabs = [
+const TABS = [
   { title: "Board View", icon: <MdGridView /> },
   { title: "List View", icon: <FaList /> },
 ];
@@ -22,7 +27,7 @@ const Tasks = () => {
   const [open, setOpen] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
   const params = useParams();
-  const status = params?.status ?? "";
+  const status = params?.status || "";
 
   return loading ? (
     <div className="py-10">
@@ -32,6 +37,32 @@ const Tasks = () => {
     <div className="w-full">
       <div className="flex items-center justify-between mb-4">
         <Title title={status ? `${status} Tasks` : "Tasks"} />
+
+        {status === "" && (
+          <Button
+            onClick={() => setOpen(true)}
+            label="Create Task"
+            icon={<IoMdAdd className="text-lg" />}
+            className="flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md py-2 2xl:py-2.5"
+          />
+        )}
+      </div>
+
+      <div>
+        <Tabs tabs={TABS} setSelected={setSelected}>
+          {!status && (
+            <div className="w-full flex justify-between gap-4 md:gap-x-12 py-4 ">
+              <TaskTitle label="A fazer " className={Task_type.todo} />
+              <TaskTitle
+                label="Em progresso"
+                className={Task_type["in progress"]}
+              />
+              <TaskTitle label="Completadas" className={Task_type.completed} />
+            </div>
+          )}
+
+          {selected === 0 ? <BoardView tasks={tasks} /> : <div></div>}
+        </Tabs>
       </div>
     </div>
   );
